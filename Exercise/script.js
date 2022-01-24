305cfabe97ed4a4935ee638b0a27169e7f47b544
Shiho Nagano <shiho@nagano.com>
2022-01-24 12:24:28 -0800

    added answer

--- Exercise/script.js
+++ Exercise/script.js
@@ -5,13 +5,25 @@
   -----------
   Highlight all of the words over 8 characters long in the paragraph text (with a yellow background for example)
 */
-
+const paragraph = document.querySelector('p')
+paragraph.style.backgroundColor = 'yellow';
 /*
   Exercise 02
   -----------
   Add a link back to the source of the text after the paragraph tag.
   (http://officeipsum.com/)
 */
+const newElement = document.createElement('a');
+const newContent = document.createTextNode('This is Link');
+
+newElement.appendChild(newContent);
+newElement.setAttribute("href", "http://officeipsum.com/");
+
+const parent = paragraph.parentElement;
+console.log(parent)
+
+parent.insertBefore(newElement, paragraph.nextSibling);
+
 
 /*
   Exercise 03
@@ -20,6 +32,18 @@
   A sentence can be assumed to be a string of text terminated with a period (.)
 */
 
+const pContents = paragraph.textContent;
+
+const split = pContents.split('.');
+
+const mapping = split.map(par => (
+  par + '.<br/>'
+))
+
+const inner = mapping.join('');
+
+paragraph.innerHTML = inner;
+
 /* 
   Exercise 04
   -----------
@@ -27,8 +51,30 @@
   You can assume that all words are separated by one singular whitespace.
 */
 
+console.log(inner.length);
+
+const newHeading = document.createElement('p');
+const newHeadingContent = document.createTextNode(inner.length);
+
+newHeading.appendChild(newHeadingContent);
+
+
+const header = document.querySelector('h1');
+
+parent.insertBefore(newHeading, header.nextSibling);
+
 /*
   Exercise 05
   -----------
   Replace all question marks (?) with thinking faces (🤔) and exclamation marks (!) with astonished faces (😲) 
 */
+
+console.log(paragraph.innerHTML)
+const str = paragraph.innerHTML;
+
+let getThinkng = str.replaceAll('?', '🤔')
+let getAstonished = getThinkng.replaceAll('!', '😲');
+
+console.log(getAstonished);
+
+paragraph.innerHTML = getAstonished;
